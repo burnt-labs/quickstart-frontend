5ab77aa588a26caaffbd9499c541b36af976459a
Sun Burnt <sun@burnt.com>
2025-04-30 18:43:36 -0400

    init

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="p-10 space-y-6">
+      <h1 className="text-3xl font-bold">web app using @my/ui and @my/utils</h1>
+    </main>
+  );
+}

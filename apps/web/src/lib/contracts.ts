bc742fdeba6fa88950a5b7db678c689c08433446
Sun Burnt <sun@burnt.com>
2025-05-06 18:07:53 -0400

    feat: hide launch if address is found. stub contract recovery

---
+++ apps/web/src/lib/contracts.ts
@@ -0,0 +1,4 @@
+import { predictInstantiate2Address } from "./predictInstantiate2Address";
+import { VERSIONS } from "../config/constants";
+
+export function findUserContracts(senderAddress: string) {}

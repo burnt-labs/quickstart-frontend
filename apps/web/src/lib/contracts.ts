472b0241cd51f8c17edf359b4a2bcfe2dabe2ad5
Sun Burnt <sun@burnt.com>
2025-05-06 18:13:11 -0400

    chore: remove contract stub

--- apps/web/src/lib/contracts.ts
+++
@@ -1,4 +0,0 @@
-import { predictInstantiate2Address } from "./predictInstantiate2Address";
-import { VERSIONS } from "../config/constants";
-
-export function findUserContracts(senderAddress: string) {}

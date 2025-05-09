8153f0cc17d79533db68244c9e91ff5fc27418b3
Sun Burnt <sun@burnt.com>
2025-05-09 12:06:16 -0400

    feat: revert test salt back to normal

--- apps/web/src/config/constants.ts
+++ apps/web/src/config/constants.ts
@@ -11,7 +11,7 @@ type Version = {
 // Track the versions of the contracts with salt for address prediction
 export const VERSIONS: Record<string, Version> = {
   "0.0.1": {
-    salt: "salt7-demo-05091",
+    salt: "salt7",
     checksums: {
       userMap:
         "9302D2D7F67A505520E78E95467D70CAA9366C7DEE2F6EE8592205A4D3B1EDD1",

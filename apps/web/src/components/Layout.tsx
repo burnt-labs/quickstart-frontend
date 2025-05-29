974bb37196ce74d222c6de18a20f843c79368fbb
Sun Burnt <sun@burnt.com>
2025-05-29 17:37:58 -0400

    feat: update landing page

--- apps/web/src/components/Layout.tsx
+++ apps/web/src/components/Layout.tsx
@@ -22,20 +22,34 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Abstraxion onClose={() => setShowModal(false)} />
-      <header className="flex justify-between items-center w-full border-b border-white/20 py-5 px-8 sm:px-24 mb-4">
-        <img className="object-contain h-8" src={xionLogo} alt="Xion Logo" />
-        <nav>
-          <BaseButton
-            className="hover:cursor-pointer"
-            onClick={handleLoginClick}
-          >
-            {client ? "Logout" : "Login"}
-          </BaseButton>
-        </nav>
-      </header>
-      <main className="flex justify-between items-center w-full py-5 px-8 sm:px-24 mb-4">
-        {client ? children : <NotLoggedIn />}
-      </main>
+      {client ? (
+        <>
+          <header className="flex justify-between items-center w-full border-b border-white/20 py-5 px-8 sm:px-24 mb-4">
+            <img
+              className="object-contain h-8"
+              src={xionLogo}
+              alt="Xion Logo"
+            />
+            <nav>
+              <BaseButton
+                className="hover:cursor-pointer"
+                onClick={handleLoginClick}
+              >
+                {client ? "Logout" : "Login"}
+              </BaseButton>
+            </nav>
+          </header>
+          <main className="flex justify-between items-center w-full py-5 px-8 sm:px-24 mb-4">
+            {children}
+          </main>
+        </>
+      ) : (
+        <>
+          <main className="w-full h-screen flex flex-col items-center justify-center">
+            <NotLoggedIn />
+          </main>
+        </>
+      )}
     </>
   );
 }
--- apps/web/src/components/NotLoggedIn.tsx
+++ apps/web/src/components/NotLoggedIn.tsx
@@ -1,6 +1,33 @@
 import { useModal } from "@burnt-labs/abstraxion";
 import { BaseButton } from "./ui/BaseButton";
-import { SubsectionTitle } from "./ui/Typography";
+import { PageTitle, SubsectionTitle } from "./ui/Typography";
+import content from "../content/landing.json";
+
+const ActionCard = ({
+  title,
+  description,
+  link,
+  link_text,
+  onClick,
+}: {
+  title: string;
+  description: string;
+  link: string;
+  link_text: string;
+  onClick?: () => void;
+}) => {
+  return (
+    <section className="flex flex-col gap-2 max-w-[375px] bg-white/5 rounded-lg p-8">
+      <SubsectionTitle>{title}</SubsectionTitle>
+      <p>{description}</p>
+      <BaseButton
+        onClick={onClick ? onClick : () => window.open(link, "_blank")}
+      >
+        {link_text}
+      </BaseButton>
+    </section>
+  );
+};
 
 export function NotLoggedIn() {
   const [, setShowModal] = useModal();
@@ -10,16 +37,30 @@ export function NotLoggedIn() {
   };
 
   return (
-    <article className="w-full mx-auto flex flex-col max-w-screen-md">
-      <header className="mb-4">
-        <h1>Welcome to XION Dev Quick Start</h1>
-        <SubsectionTitle>Login to get started</SubsectionTitle>
-      </header>
-      <section className="w-full bg-white/5 rounded-lg p-8">
-        <BaseButton className="w-full" onClick={handleLoginClick}>
-          Login
-        </BaseButton>
-      </section>
+    <article className="flex flex-col items-center">
+      <PageTitle>{content.title}</PageTitle>
+      <SubsectionTitle className="mb-8">{content.tagline}</SubsectionTitle>
+      <div className="flex gap-4">
+        {content.action_cards.map((card, index) => (
+          <ActionCard
+            key={index}
+            {...card}
+            onClick={index === 1 ? handleLoginClick : undefined}
+          />
+        ))}
+      </div>
     </article>
   );
 }
+
+// <article className="w-full mx-auto flex flex-col max-w-screen-md">
+// <header className="mb-4">
+//   <h1>Welcome to XION Dev Quick Start</h1>
+//   <SubsectionTitle>Login to get started</SubsectionTitle>
+// </header>
+// <section className="w-full bg-white/5 rounded-lg p-8">
+//   <BaseButton className="w-full" onClick={handleLoginClick}>
+//     Login
+//   </BaseButton>
+// </section>
+// </article>
